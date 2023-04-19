5249cf498ca81fd2133b8335d79b3a691353f2ed
Nosova Olga <olga.trif@mail.ru>
2023-04-19 13:36:22 +0500

    К выводу простых чисел добавили порядковый номер числа

--- prime_num.py
+++ prime_num.py
@@ -3,12 +3,14 @@ import math
 prime = [2, 3]
 
 n = int(input('Введите число, до которого следует вывести простые числа:\n'))
+print()
 
 while n < 2:
     n = int(input('Введите число не меньше двух:\n'))
+    print()
 
 if n == 2:
-    print(2)
+    print('1: 2')
     exit(0)
 
 for num in range(5, n + 1):
@@ -21,5 +23,7 @@ for num in range(5, n + 1):
             prime.append(num)
             break
 
-[print(num) for num in prime]
-
+i = 1
+for num in prime:
+    print(i, num, sep=': ')
+    i += 1
